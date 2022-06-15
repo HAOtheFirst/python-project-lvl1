3ac554bae300ad93194ace554df99ac3929c980b
Gusev <gysev170296@gmail.com>
2022-06-16 00:04:35 +0300

    add game_calc and script for game_calc

---
+++ brain_games/games/game_calc.py
@@ -0,0 +1,31 @@
+from brain_games.cli import welcome_user
+import random
+import prompt
+
+
+def game_calc():
+    name = welcome_user()
+    print('What is the result of the expression?')
+    operator = "+-*"
+    i = 0
+
+    while i < 3:
+        number_one = random.randint(1, 100)
+        number_two = random.randint(1, 100)
+        operation = random.choice(operator)
+
+        print(f'Question: {number_one} {operation} {number_two}')
+
+        correct_answer = eval(f'{number_one} {operation} {number_two}')
+
+        answer = int(prompt.string('Your answer: '))
+
+        if answer == correct_answer:
+            print('Correct!')
+            i += 1
+        else:
+            return print(f"'{answer}' is wrong answer ;(. Correct answer was "
+                         f"'{correct_answer}'.'\n Let's try again, {name}!")
+
+    if i == 3:
+        print(f'Congratulations, {name}!')
---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from brain_games.games.game_calc import game_calc
+
+
+def main():
+    game_calc()
+
+
+if __name__ == "__main__":
+    main()
