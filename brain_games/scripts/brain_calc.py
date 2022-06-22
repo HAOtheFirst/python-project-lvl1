eb0d8eae3d5f773d4e1fd49865fd555b063a5144
Gusev <gysev170296@gmail.com>
2022-06-22 19:48:26 +0300

    Revert "fix game_calc"
    This reverts commit b248e68f394f7ec96256f0a873e19cf07ad1b98e.

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 from brain_games.games.game_calc import game_calc
-from ..cli import welcome_user
 
 
 def main():
-    name = welcome_user()
-    print('What is the result of the expression?')
-    game_calc(name)
+    game_calc()
 
 
 if __name__ == "__main__":
