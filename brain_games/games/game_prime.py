9e57c866919cf6b70f1b08605f95163ad49fc2ea
Gusev <gysev170296@gmail.com>
2022-06-24 00:46:48 +0300

    update game_prime.py

--- brain_games/games/game_prime.py
+++ brain_games/games/game_prime.py
@@ -2,17 +2,17 @@
 from ..cli import welcome_user, get_prime_list, is_done, start_game
 import random
 import prompt
+import time
 
 
 def game_prime():
     name = welcome_user()
     print('Answer "yes" if given number is prime. Otherwise answer "no".')
     start_game(prime, name, 2)
-    get_prime_list(500)
 
 
 def prime(name):
-    prime_list, max_range = get_prime_list(500)
+    prime_list, max_range = get_prime_list(100000)
     number = random.randint(1, max_range)
     print(f'Question: {number}')
     answer = prompt.string('Your answer: ')
