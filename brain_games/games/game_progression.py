a12034b9655fcba6cd5dd38743ec69a27bdd1680
Gusev <gysev170296@gmail.com>
2022-06-23 19:53:42 +0300

    update game_progression

--- brain_games/games/game_progression.py
+++ brain_games/games/game_progression.py
@@ -1,4 +1,4 @@
-from brain_games.cli import welcome_user
+from brain_games.cli import welcome_user, start_game, is_done
 import random
 import prompt
 
@@ -6,37 +6,32 @@ import prompt
 def game_progression():
     name = welcome_user()
     print('What number is missing in the progression?')
+    start_game(progression, name, 2)
 
-    i = 0
 
-    while i < 3:
-        value = random.randint(1, 10)
-        d = random.randint(1, 10)
-        len_progression = random.randint(5, 10)
-        progression = []
-        for a in range(0, len_progression):
-            nam = value + (a * d)
-            progression.append(nam)
+def progression(name):
+    value = random.randint(1, 10)
+    d = random.randint(1, 10)
+    len_progression = random.randint(5, 10)
+    progression_list = []
+    for a in range(0, len_progression):
+        nam = value + (a * d)
+        progression_list.append(nam)
 
-        element = random.randint(0, len_progression - 1)
-        correct_answer = progression[element]
-        progression.remove(progression[element])
-        progression.insert(element, '..')
+    element = random.randint(0, len_progression - 1)
+    correct_answer = progression_list[element]
+    progression_list.remove(progression_list[element])
+    progression_list.insert(element, '..')
 
-        progression_string = ''
+    progression_string = ''
 
-        for char in progression:
-            progression_string += f'{char} '
+    for char in progression_list:
+        progression_string += f'{char} '
 
-        print(f'Question: {progression_string}')
-        answer = prompt.real('Your answer: ')
+    print(f'Question: {progression_string}')
+    answer = prompt.real('Your answer: ')
 
-        if answer == correct_answer:
-            print('Correct!')
-            i += 1
-        else:
-            return print(f"'{answer}' is wrong answer ;(. Correct answer was "
-                         f"'{correct_answer}'.'\n Let's try again, {name}!")
-
-    if i == 3:
-        print(f'Congratulations, {name}!')
+    if is_done(answer, correct_answer, name) is True:
+        return True
+    else:
+        return False
